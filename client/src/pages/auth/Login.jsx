37a5a2e9ba3409c2d7c94cd1558ced55db08c686
ram <ram.poonia@ancora.tech>
2024-07-26 17:54:46 +0530

    fix: error handling for login

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -10,9 +10,9 @@ export default function Login() {
     const location = useLocation()
     const fromLocation = location?.state?.from?.pathname || '/'
     const [loading, setLoading] = useState(false)
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
-    
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [errors, setError] = useState({})
     function onEmailChange(event) {
         setEmail(event.target.value)
     }
@@ -23,28 +23,40 @@ export default function Login() {
 
     async function onSubmitForm(event) {
         event.preventDefault()
-
-        setLoading(true)
-
-        try {
-            const {data, headers} = await axiosInstance.post('auth/login', JSON.stringify({
-                email,
-                password
-            })) ?? {access_token: ''}
-            localStorage.setItem('access-token', data?.access_token)
-            localStorage.setItem('refresh_token', data.refresh_token)
-            setAccessToken(data?.access_token)
-            setCSRFToken(headers["x-csrftoken"])
-            setIsLoggedIn(true);
-            setEmail()
-            setPassword()
-            setLoading(false)
-
-            navigate('/hello_world')
-            // navigate(fromLocation, { replace: true })
-        } catch (error) {
-            setLoading(false)
-            // TODO: handle errors
+        let valid = true
+        const error = {}
+        if(email === '') {
+            error['email'] = ['Email Field is mandatory']
+            valid = false
+        }
+        if(password === '') {
+            error['password'] = ['Password is mandatory']
+            valid = false
+        }
+        setError(error)
+        if(valid) {
+            setLoading(true)
+            try {
+                const {data, headers} = await axiosInstance.post('auth/login', JSON.stringify({
+                    email,
+                    password
+                })) ?? {access_token: ''}
+                localStorage.setItem('access-token', data?.access_token)
+                localStorage.setItem('refresh_token', data.refresh_token)
+                setAccessToken(data?.access_token)
+                setCSRFToken(headers["x-csrftoken"])
+                setIsLoggedIn(true);
+                setEmail()
+                setPassword()
+                setLoading(false)
+                
+                navigate('/hello_world')
+                // navigate(fromLocation, { replace: true })
+            } catch (error) {
+                console.log("=============", error.response.data)
+                setLoading(false)
+                setError(error.response.data)
+            }
         }
     }
 
@@ -54,11 +66,14 @@ export default function Login() {
             <form onSubmit={onSubmitForm}>
                 <div className="mb-3">
                     <input type="email" placeholder='Email' autoComplete='off' className='form-control' id="email" onChange={onEmailChange} />
+                    {errors.email && <div className="invalid-feedback" style={{display: 'block'}}>{errors.email[0]}</div>}
                 </div>
                 <div className="mb-3">
                     <input type="password" placeholder='Password' autoComplete='off' className='form-control' id="password" onChange={onPasswordChange} />
+                    {errors.password && <div className="invalid-feedback" style={{display: 'block'}}>{errors.password[0]}</div>}
                 </div>
                 <div className="mb-3">
+                    {errors.detail && <div className="invalid-feedback" style={{display: 'block'}}>{errors.detail}</div>}
                     <button disabled={loading} className='btn btn-success' type="submit">Login</button>
                 </div>
             </form>
